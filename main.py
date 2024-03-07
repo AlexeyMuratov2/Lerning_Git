c66027d5fc7809ee01cec67f2c8481688b5c5d5f
MuratovAlexey <alexey.e.muratov@gmail.com>
2024-03-07 14:44:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+class Dog:
+    def bark(self) -> None:
+        pass
+
+
+class Cat:
+    def meow(self) -> None:
+        pass
+
+
+class Cow:
+    def moo(self) -> None:
+        pass
+
+
+class Car:
+    def __init__(self, mark: str):
+        pass
+
+    def drive(self) -> None:
+        pass
+
+
+class Engine(Car):
+    def repair(self) -> None:
+        pass
+
+
+def get_user_data(row_num: int) -> str:
+    pass
+
+
+def summ(a: int, b: int) -> int:
+    pass
+
+
+def mul(a: int, b: int) -> int:
+    pass
+
+
+def sub(a: int, b: int) -> int:
+    pass
+
+
+def div(a: int, b: int) -> int:
+    pass
